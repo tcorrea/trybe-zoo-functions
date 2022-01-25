612d491c3b3b8b6478d844c56cb7076f211194e2
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-01-25 17:36:54 -0300

    Requisito 1 ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return data.species.reduce((acc, current) => {
+    ids.forEach((element) => { if (current.id === element) acc.push(current); });
+    return acc;
+  }, []);
 }
-
 module.exports = getSpeciesByIds;
