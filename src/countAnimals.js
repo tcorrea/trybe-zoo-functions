8b95f411b183d097d000d73c1c40fe8239797f1e
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-01-26 16:44:50 -0300

    Requisito 5 ok

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (typeof animal === 'undefined') {
+    return data.species.reduce((acc, current) => {
+      acc[current.name] = current.residents.length;
+      return acc;
+    }, {});
+  }
+
+  const animalFound = data.species.find((element) => element.name === animal.specie);
+
+  if ('sex' in animal) {
+    return animalFound.residents.filter((element) => element.sex === 'female').length;
+  }
+  return animalFound.residents.length;
 }
 
 module.exports = countAnimals;
