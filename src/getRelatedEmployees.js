5cb0f378ef096d52b4d63ea7506b4b71bfb74c9d
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-01-26 16:13:26 -0300

    Requisito 4 ok

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees
+    .map((elemet) => elemet.managers.includes(id))
+    .some((element) => element === true);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return data.employees.reduce((acc, current) => {
+      if (current.managers.includes(managerId)) {
+        acc.push(`${current.firstName} ${current.lastName}`);
+      }
+      return acc;
+    }, []);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
