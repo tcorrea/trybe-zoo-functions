90fabc71907f69f69c6b2f7540107573b1801ce8
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-01-27 19:28:29 -0300

    Requisito 666 (7) ok - Lint not ok

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -31,31 +31,31 @@ function animalsByLocation(objLocation) {
 
 function getAnimalMap(options) {
   const objLocation = { NE: [], NW: [], SE: [], SW: [] };
-
   if (typeof options === 'undefined') {
     return animalsByLocation(objLocation);
   }
-  if (options.sex === 'female' && !('includeNames' in options) && !('sorted' in options)) {
-    return animalsByLocation(objLocation);
+
+  if (('includeNames' in options) && ('sex' in options)){
+    return animalsBySex(objLocation, options.sex, options.sorted);
   }
-  if (!('includeNames' in options) && options.sex === 'female' && options.sorted === true) {
-    return animalsByLocation(objLocation);
-  }
+
   if (options.includeNames === true && !('sex' in options)) {
     return getSpeciesByLocation(objLocation, options.sorted);
   }
-  return animalsBySex(objLocation, options.sex, options.sorted);
+ 
+  return animalsByLocation(objLocation);
+  // if (options.sex === 'female' && !('includeNames' in options) && !('sorted' in options)) {
+  //   return animalsByLocation(objLocation);
+  // }
+  // if (!('includeNames' in options) && options.sex === 'female' && options.sorted === true) {
+  //   return animalsByLocation(objLocation);
+  // }
+  
+  
 }
 
-// ✓ sem parâmetros, retorna animais categorizados por localização (2ms)
-// ✓ sem a opção `includeNames` especificada e somente com a opção `sex: female` especificada, retorna todos os animais categorizados por localização sem aplicar o filtro `sex`
-// ✓ sem a opção `includeNames` especificada e as opções `sex: female` e `sorted: true` forem especificadas, retorna animais categorizados por localização sem aplicar os filtros `sex` e `sorted`
-// ✓ com a opção `includeNames: true` especificada, retorna nomes de animais (1ms)
-// ✓ com a opção `sorted: true` especificada, retorna nomes de animais ordenados (5ms)
-// ✕ com a opção `sex: 'female'` ou `sex: 'male'` especificada, retorna somente nomes de animais macho/fêmea (6ms)
-// ✕ com a opção `sex: 'female'` ou `sex: 'male'` es
-
-const t = animalsBySex({ NE: [], NW: [], SE: [], SW: [] }, 'female', true);
-console.log(t.NE);
+console.log(getAnimalMap());
 
 module.exports = getAnimalMap;
+// { includeNames: true, sex: 'female', sorted: true }
+// { includeNames: true, sex: 'female' };
