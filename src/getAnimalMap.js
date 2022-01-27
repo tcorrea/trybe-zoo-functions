19c4a944bc38bbd16c566899b5c519fbbc348a7e
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-01-27 19:18:18 -0300

    Requisito 666 (7) ok - Lint not ok

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,61 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function isSorted(array, sorted = false) { return (sorted) ? array.sort() : array; }
+
+function getSpeciesByLocation(objLocation, sorted = false) {
+  return data.species.reduce((acc, specie) => {
+    acc[specie.location].push(
+      { [specie.name]: isSorted(specie.residents.map((element) => element.name), sorted) },
+    );
+    return acc;
+  }, objLocation);
 }
+
+function animalsBySex(objLocation, sex, sorted = false) {
+  return data.species.reduce((acc, specie) => {
+    acc[specie.location].push({ [specie.name]: isSorted(
+      specie.residents.filter((element) => element.sex === sex).map((item) => item.name), sorted,
+    ) });
+    return acc;
+  }, objLocation);
+}
+
+function animalsByLocation(objLocation) {
+  return data.species.reduce((acc, specie) => {
+    acc[specie.location] = data.species.filter((element) => {
+      return element.location === specie.location;
+    }).map((item) => item.name);
+    return acc;
+  }, objLocation);
+}
+
+function getAnimalMap(options) {
+  const objLocation = { NE: [], NW: [], SE: [], SW: [] };
+
+  if (typeof options === 'undefined') {
+    return animalsByLocation(objLocation);
+  }
+  if (options.sex === 'female' && !('includeNames' in options) && !('sorted' in options)) {
+    return animalsByLocation(objLocation);
+  }
+  if (!('includeNames' in options) && options.sex === 'female' && options.sorted === true) {
+    return animalsByLocation(objLocation);
+  }
+  if (options.includeNames === true && !('sex' in options)) {
+    return getSpeciesByLocation(objLocation, options.sorted);
+  }
+  return animalsBySex(objLocation, options.sex, options.sorted);
+}
+
+// ✓ sem parâmetros, retorna animais categorizados por localização (2ms)
+// ✓ sem a opção `includeNames` especificada e somente com a opção `sex: female` especificada, retorna todos os animais categorizados por localização sem aplicar o filtro `sex`
+// ✓ sem a opção `includeNames` especificada e as opções `sex: female` e `sorted: true` forem especificadas, retorna animais categorizados por localização sem aplicar os filtros `sex` e `sorted`
+// ✓ com a opção `includeNames: true` especificada, retorna nomes de animais (1ms)
+// ✓ com a opção `sorted: true` especificada, retorna nomes de animais ordenados (5ms)
+// ✕ com a opção `sex: 'female'` ou `sex: 'male'` especificada, retorna somente nomes de animais macho/fêmea (6ms)
+// ✕ com a opção `sex: 'female'` ou `sex: 'male'` es
+
+const t = animalsBySex({ NE: [], NW: [], SE: [], SW: [] }, 'female', true);
+console.log(t.NE);
 
 module.exports = getAnimalMap;
