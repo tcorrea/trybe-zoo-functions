cbcb592ca7e7cbca40cba1378dbf02f597deedeb
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-01-28 18:42:07 -0300

    Requisito 9 ok

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,61 +1,60 @@
 const data = require('../data/zoo_data');
 
-function isSorted(array, sorted = false) { return (sorted) ? array.sort() : array; }
+const isSorted = (array, sorted = false) => ((sorted) ? array.sort() : array);
 
-function getSpeciesByLocation(objLocation, sorted = false) {
+// function speciesByLocation(objLocation, sorted = false) {
+//   return data.species.reduce((acc, specie) => {
+//     acc[specie.location].push({ [specie.name]: isSorted(specie.residents.map((element) => element.name), sorted) });
+//     return acc;
+//   }, objLocation);
+// }
+
+// function animalsBySex(objLocation, sex, sorted = false) {
+//   return data.species.reduce((acc, specie) => {
+//     acc[specie.location].push({ [specie.name]: isSorted(specie.residents.filter((element) => element.sex === sex).map((item) => item.name), sorted) });
+//     return acc;
+//   }, objLocation);
+// }
+
+function speciesByLocation(objLocation, sex, sorted = false) {
   return data.species.reduce((acc, specie) => {
-    acc[specie.location].push(
-      { [specie.name]: isSorted(specie.residents.map((element) => element.name), sorted) },
-    );
+    if (sex) {
+      const varsex = specie.residents.filter((element) => element.sex === sex).map((item) => item.name);
+      acc[specie.location].push({ [specie.name]: isSorted(varsex, sorted) });
+    } else {
+      const names = specie.residents.map((element) => element.name);
+      acc[specie.location].push({ [specie.name]: isSorted(names, sorted) });
+    }
     return acc;
   }, objLocation);
 }
 
-function animalsBySex(objLocation, sex, sorted = false) {
+function animalsNamesByLocation(objLocation) {
   return data.species.reduce((acc, specie) => {
-    acc[specie.location].push({ [specie.name]: isSorted(
-      specie.residents.filter((element) => element.sex === sex).map((item) => item.name), sorted,
-    ) });
-    return acc;
-  }, objLocation);
-}
-
-function animalsByLocation(objLocation) {
-  return data.species.reduce((acc, specie) => {
-    acc[specie.location] = data.species.filter((element) => {
-      return element.location === specie.location;
-    }).map((item) => item.name);
+    acc[specie.location] = data.species.filter(
+      (element) => element.location === specie.location,
+    ).map((item) => item.name);
     return acc;
   }, objLocation);
 }
 
 function getAnimalMap(options) {
   const objLocation = { NE: [], NW: [], SE: [], SW: [] };
-  if (typeof options === 'undefined') {
-    return animalsByLocation(objLocation);
-  }
-
-  if (('includeNames' in options) && ('sex' in options)){
-    return animalsBySex(objLocation, options.sex, options.sorted);
-  }
-
-  if (options.includeNames === true && !('sex' in options)) {
-    return getSpeciesByLocation(objLocation, options.sorted);
-  }
- 
-  return animalsByLocation(objLocation);
-  // if (options.sex === 'female' && !('includeNames' in options) && !('sorted' in options)) {
-  //   return animalsByLocation(objLocation);
-  // }
-  // if (!('includeNames' in options) && options.sex === 'female' && options.sorted === true) {
-  //   return animalsByLocation(objLocation);
-  // }
-  
-  
+  { includeNames, sex, sorted } = options;
 }
 
-console.log(getAnimalMap());
-
 module.exports = getAnimalMap;
-// { includeNames: true, sex: 'female', sorted: true }
-// { includeNames: true, sex: 'female' };
+
+// if (typeof options === 'undefined') {
+//   return animalsNamesByLocation(objLocation);
+// }
+// if (!('includeNames' in options)) {
+//   animalsNamesByLocation(objLocation);
+// }
+// if (('includeNames' in options) && ('sex' in options)) {
+//   return speciesByLocation(objLocation, options.sex, options.sorted);
+// }
+// if (options.includeNames === true && !('sex' in options)) {
+//   return speciesByLocation(objLocation, options.sorted);
+// }
+// return animalsNamesByLocation(objLocation);
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstSpecieId = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+
+  const specieFound = data.species.find((specie) => specie.id === firstSpecieId);
+
+  return Object.values(
+    specieFound.residents.reduce((acc, curr) => ((acc.age < curr.age) ? curr : acc)),
+  );
 }
 
 module.exports = getOldestFromFirstSpecies;
