d044636d7a13584e41724781954b1f61bf4b6c4d
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-01-30 12:29:21 -0300

    Requisito 7 ok

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,62 +1,41 @@
 const data = require('../data/zoo_data');
 
-// const isSorted = (array, sorted = false) => ((sorted) ? array.sort() : array);
+const isSorted = (array, sorted = false) => ((sorted) ? array.sort() : array);
 
-// ----------------------- APAGAR
-// function speciesByLocation(objLocation, sorted = false) {
-//   return data.species.reduce((acc, specie) => {
-//     acc[specie.location].push({ [specie.name]: isSorted(specie.residents.map((element) => element.name), sorted) });
-//     return acc;
-//   }, objLocation);
-// }
-
-// function animalsBySex(objLocation, sex, sorted = false) {
-//   return data.species.reduce((acc, specie) => {
-//     acc[specie.location].push({ [specie.name]: isSorted(specie.residents.filter((element) => element.sex === sex).map((item) => item.name), sorted) });
-//     return acc;
-//   }, objLocation);
-// }
-// ----------------------- APAGAR
-
-// function speciesByLocation(objLocation, sex, sorted = false) {
-//   return data.species.reduce((acc, specie) => {
-//     if (sex) {
-//       const varsex = specie.residents.filter((element) => element.sex === sex).map((item) => item.name);
-//       acc[specie.location].push({ [specie.name]: isSorted(varsex, sorted) });
-//     } else {
-//       const names = specie.residents.map((element) => element.name);
-//       acc[specie.location].push({ [specie.name]: isSorted(names, sorted) });
-//     }
-//     return acc;
-//   }, objLocation);
-// }
-
-// function animalsNamesByLocation(objLocation) {
-//   return data.species.reduce((acc, specie) => {
-//     acc[specie.location] = data.species.filter(
-//       (element) => element.location === specie.location,
-//     ).map((item) => item.name);
-//     return acc;
-//   }, objLocation);
-// }
-
-function getAnimalMap(options) {
-  // const objLocation = { NE: [], NW: [], SE: [], SW: [] };
-  // { includeNames, sex, sorted } = options;
+function speciesByLocation(objLocation, sex, sorted = false) {
+  return data.species.reduce((acc, specie) => {
+    if (sex) {
+      const sexFilter = specie.residents.filter(
+        (element) => element.sex === sex,
+      ).map((item) => item.name);
+      acc[specie.location].push({ [specie.name]: isSorted(sexFilter, sorted) });
+    } else {
+      const names = specie.residents.map((element) => element.name);
+      acc[specie.location].push({ [specie.name]: isSorted(names, sorted) });
+    }
+    return acc;
+  }, objLocation);
 }
 
-module.exports = getAnimalMap;
+function animalsNamesByLocation(objLocation) {
+  return data.species.reduce((acc, specie) => {
+    acc[specie.location] = data.species.filter(
+      (element) => element.location === specie.location,
+    ).map((item) => item.name);
+    return acc;
+  }, objLocation);
+}
 
-// if (typeof options === 'undefined') {
-//   return animalsNamesByLocation(objLocation);
-// }
-// if (!('includeNames' in options)) {
-//   animalsNamesByLocation(objLocation);
-// }
-// if (('includeNames' in options) && ('sex' in options)) {
-//   return speciesByLocation(objLocation, options.sex, options.sorted);
-// }
-// if (options.includeNames === true && !('sex' in options)) {
-//   return speciesByLocation(objLocation, options.sorted);
-// }
-// return animalsNamesByLocation(objLocation);
+function getAnimalMap(options) {
+  const objLocation = { NE: [], NW: [], SE: [], SW: [] };
+  if (typeof options === 'undefined' || !('includeNames' in options)) {
+    return animalsNamesByLocation(objLocation);
+  }
+
+  if (options.includeNames === true) {
+    return speciesByLocation(objLocation, options.sex, options.sorted);
+  }
+}
+
+console.log(getAnimalMap());
+module.exports = getAnimalMap;
